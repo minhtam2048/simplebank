2656fd290e4178e6382f2dd6fbab2b48c281d208
minhtam2048 <minhtam2048@gmail.com>
2024-03-10 18:44:58 +0900

    Add unit test for account

---
+++ util/random.go
@@ -0,0 +1,40 @@
+package util
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const alphabet = "weiouyasdkcnbzmnasD"
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// RandomInt generates a random integer between min and max
+func RandomInt(min, max int64) int64 {
+	return min + rand.Int63n(max-min+1) // 0 ->max-min
+}
+
+func RandomString(n int) string {
+	var sb strings.Builder
+	k := len(alphabet)
+
+	for i := 0; i < n; i++ {
+		c := alphabet[rand.Intn(k)]
+		sb.WriteByte(c)
+	}
+
+	return sb.String()
+}
+
+func RandomOwner() string {
+	return RandomString(6)
+}
+
+func RandomCurrency() string {
+	currencies := []string{"EUR, USD"}
+	n := len(currencies)
+	return currencies[rand.Intn(n)]
+}
